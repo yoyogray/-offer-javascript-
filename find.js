5e8da07612a0fcc6c79e910eb0bf726ee0fe705d
yuegray <474851714@qq.com>
2017-10-19 16:30:17 +0800

    init

---
+++ find.js
@@ -0,0 +1,23 @@
+function find(target,arr){
+    var row = arr.length;
+    var col = array[0].length;
+    var r = row - 1;
+    var c = 0;
+    while (r>=0 && c<=col-1){
+        if(target > array[r][c]){
+            c++
+        }
+        else if(target < array[r][c]){
+            r--;
+        }else{
+            console.log('true')
+            return true
+        }
+        console.log('false')
+        return false;
+    }
+}
+
+var tar = 10;
+var arr = [[1,2,3],[10,22,33],[44,32,52]]
+find(tar,arr)
